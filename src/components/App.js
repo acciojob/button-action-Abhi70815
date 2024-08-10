92e15c7498f3a134b91ca17a9d125c167b6d80e9
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-08-10 22:34:41 +0530

    save

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
-
+const [classn, setClass]=useState("hide");
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <p id="para" className={classn}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <button id="click" onClick={()=>setClass("show")}>Show</button>
+      
     </div>
   );
 }
